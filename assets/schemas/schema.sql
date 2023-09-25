d6eba3a3af397154fd0cd06c94f5e79f01127f0a
Anthony Iacano <iacano5@outlook.com>
2023-09-25 13:19:16 -0400

    added schema.sql and created basic structure to match img found in assets/img, added seeds.sql

---
+++ assets/schemas/schema.sql
@@ -0,0 +1,26 @@
+-- Create the department table
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+-- Create the role table
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+);
+
+-- Create the employee table
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    -- manager_id will be null if employee does not have a manager
+);
